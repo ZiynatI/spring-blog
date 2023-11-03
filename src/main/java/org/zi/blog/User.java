46ff7f59b47c918d27335846c8b341748b1a4e88
ZiynatII <zisomurodova@gmail.com>
2023-11-03 13:06:16 +0500

    Word.java and User.java created with JPA

--- src/main/java/org/zi/blog/User.java
+++ src/main/java/org/zi/blog/User.java
@@ -1,9 +1,19 @@
 package org.zi.blog;
 
+import jakarta.persistence.*;
 import org.zi.blog.word.Word;
 
 import java.util.List;
-
+@Entity
+@Table(name = "user")
 public class User {
-    List<Word> listOfWords;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Word> words;
+
 }
--- src/main/java/org/zi/blog/word/Word.java
+++ src/main/java/org/zi/blog/word/Word.java
@@ -1,8 +1,30 @@
 package org.zi.blog.word;
 
+import jakarta.persistence.*;
+import org.zi.blog.User;
 import org.zi.blog.word.WordStatus;
 
-public record Word(String wordContent, WordStatus wordStatus) {
+@Entity
+@Table(name = "word")
+public class Word {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String wordContent;
+    WordStatus wordStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+    public Word(String wordContent, WordStatus wordStatus, User user) {
+        this.wordContent = wordContent;
+        this.wordStatus = wordStatus;
+        this.user = user;
+    }
 
     public Word(String wordContent, WordStatus wordStatus) {
         this.wordContent = wordContent;
